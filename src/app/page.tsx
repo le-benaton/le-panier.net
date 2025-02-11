c99124d822dbd3596439c51c03bb78883b832b02
榊原昌彦 <sakakibara@rdlabo.jp>
2025-02-11 09:40:56 +0900

    feat(): 名刺からの検索に対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Logo from '@/app/logo';
+import SelectDialog from '@/app/select-dialog';
 
 export default function Home() {
   return (
     <>
+      <SelectDialog />
       <header
         className="relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3 dark:bg-neutral-800">
         <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
