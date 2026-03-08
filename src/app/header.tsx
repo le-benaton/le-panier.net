ba6a4072ad3d9cf282d4b5150251a1c7794f82f2
chiilog <purgexpurge@gmail.com>
2026-03-08 22:28:22 +0900

    feat(): ヘッダーコンポーネントを新規作成
    - Next.js用のヘッダーコンポーネントを追加
    - ロゴリンクとナビゲーションを含むレスポンシブデザインを実装
    - トップページかどうかに応じたロゴの表示を調整

---
+++ src/app/header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Logo from "@/app/logo";
+
+interface HeaderProps {
+  isTopPage?: boolean;
+}
+
+export default function Header({ isTopPage = false }: HeaderProps) {
+  const logoLink = (
+    <Link
+      className="inline-flex items-center gap-x-2 flex-none text-xl font-semibold dark:text-white focus:outline-none focus:opacity-80"
+      href="/"
+      aria-label={isTopPage ? "Brand" : "トップページに戻る"}
+    >
+      <Logo className="w-40 h-auto fill-black dark:fill-gray-50" />
+      <span className="sr-only">le panier（ル パニエ）</span>
+    </Link>
+  );
+
+  return (
+    <header className="relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3 dark:bg-neutral-800">
+      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+        <div className="flex items-center justify-between">
+          {isTopPage ? <h1>{logoLink}</h1> : logoLink}
+        </div>
+      </nav>
+    </header>
+  );
+}
